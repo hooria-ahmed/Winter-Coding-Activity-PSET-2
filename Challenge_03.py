5db42e42758e42664e6d648a5783ec7137c937b0
Hooria Ahmed <hooria.2006.haq@gmail.com>
2026-01-09 15:17:06 +0000

    Optmized code

--- Challenge_03.py
+++ Challenge_03.py
@@ -1,19 +1,24 @@
 def find_longest_mirror_length(s: str) -> int:
-    def helper(i, j):
-        # Base cases
-        if i > j:
-            return 0
-        if i == j:
-            return 1
+    n = len(s)
 
-        # Characters match
-        if s[i] == s[j]:
-            return 2 + helper(i + 1, j - 1)
+    # dp[i][j] = LPS length from i to j
+    dp = [[0] * n for _ in range(n)]
 
-        # Characters do not match
-        return max(helper(i + 1, j), helper(i, j - 1))
+    # Single characters
+    for i in range(n):
+        dp[i][i] = 1
 
-    return helper(0, len(s) - 1)
+    # Fill table bottom-up
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+
+            if s[i] == s[j]:
+                dp[i][j] = 2 + (dp[i + 1][j - 1] if length > 2 else 0)
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+    return dp[0][n - 1]
 
 
 
